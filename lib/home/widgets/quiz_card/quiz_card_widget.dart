645f4d2d363f18bd133e3064decd6e41330e8e38
Leonardo Lissone <leonardo.lissonez@gmail.com>
2021-04-21 16:58:06 -0300

    challenge page with awnsers

--- lib/home/widgets/quiz_card/quiz_card_widget.dart
+++ lib/home/widgets/quiz_card/quiz_card_widget.dart
@@ -1,6 +1,7 @@
 import 'package:DevQuiz/core/app_colors.dart';
 import 'package:DevQuiz/core/app_images.dart';
 import 'package:DevQuiz/core/app_text_styles.dart';
+import 'package:DevQuiz/shared/widgets/progress_indicator/progress_indicator_widget.dart';
 import 'package:flutter/material.dart';
 
 class QuizCardWidget extends StatelessWidget {
@@ -33,11 +34,7 @@ class QuizCardWidget extends StatelessWidget {
               SizedBox(width: 16),
               Expanded(
                 flex: 4,
-                child: LinearProgressIndicator(
-                    value: 0.3,
-                    backgroundColor: AppColors.chartSecondary,
-                    valueColor:
-                        AlwaysStoppedAnimation<Color>(AppColors.chartPrimary)),
+                child: ProgressIndicatorWidget(value: 0.3)
               )
             ],
           )
